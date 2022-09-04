85e0b48c90606b23c6147a9d3a9ea6684fe69799
fauzhanFARTF <fauzan1812@gmail.com>
2022-09-04 16:16:04 +0700

    2. Membuat Web E Commerce dengan Javascript - Exercise3 (repair)

--- 2. Membuat Web E Commerce dengan Javascript/exercise3/script.js
+++ 2. Membuat Web E Commerce dengan Javascript/exercise3/script.js
@@ -1,109 +1,135 @@
-class Kursus{
-    constructor (paket, jenis, harga){
-        this.paket = paket;
-        this.jenis = jenis;
+document.body.style.backgroundColor = "rgb(240,255,255)"
+const row = document.querySelector('.row');
+const row_colmd8 = document.querySelector('.row .col-md-8');
+
+row_colmd8.classList.add('wrapper');
+
+const wrapper = document.querySelector('.row .wrapper');
+
+wrapper.style.backgroundColor = 'white';
+
+// const table =document.querySelector('.table');
+const table = document.getElementsByTagName('table')[0];
+table.style.color = 'white';
+table.style.border = "";
+
+
+class Produk{
+    constructor (nama, harga, kategori ){
+        this.nama = nama;
         this.harga = harga;
+        this.kategori = kategori;
     };
     // constructor --> class yang perama kali dijalankan
 };
 
+// const length_table = document.getElementsByTagName('table').length;
+
+
 class UI {
-    simpanData(kursus){
-        const list = document.getElementById('data-paket');
+    simpanData(produk){
+        const list = document.getElementById('data-produk');
         const row = document.createElement('tr');
+        let table = document.getElementById('data-collection');
+        let num = table.rows.length;
+
         row.innerHTML = `
-            <td>${kursus.paket}</td>
-            <td>${kursus.jenis}</td>
-            <td>${kursus.harga}</td>
+            <td>${num}</td>
+            <td>${produk.nama}</td>
+            <td>Rp. ${produk.harga}</td>
+            <td>${produk.kategori}</td>
             <td><a href="#" class="delete">X</a></td>
             `;
         list.appendChild(row);
-    }
+    };
     clearFields(){
-        document.getElementById('paket').value = "";
-        document.getElementById('jenis').value = "";
+        document.getElementById('nama').value = "";
         document.getElementById('harga').value = "";
-    }
-    deleteKursus(target){
+        document.getElementById('kategori').value = "";
+    };
+    deleteProduk(target){
         if (target.className === "delete") {
             target.parentElement.parentElement.remove();
-        }
-    }
+        };
+    };
 };
 
-class paketKursus{
-    static getPaket(){
-        let belajar;
-        if (localStorage.getItem('belajar') === null){
-            belajar = [];
-        } else {
-            belajar = JSON.parse(localStorage.getItem('belajar'));
-        }
-        return belajar;
-    };
-    static tampilKursus(){
-        const belajar = paketKursus.getPaket();
 
-        belajar.forEach(function(kursus){
+class paketProduk{
+    static getPaket(){
+        let produk_exercise;
+        if (localStorage.getItem('produk_exercise') === null){
+            produk_exercise = [];
+        } else {
+            produk_exercise = JSON.parse(localStorage.getItem('produk_exercise'));
+        }
+        return produk_exercise;
+    };
+    static tampilProduk(){
+        const produk_exercise = paketProduk.getPaket();
+
+        produk_exercise.forEach(function(produk){
             const ui = new UI;
-            ui.simpanData(kursus);
+            ui.simpanData(produk);
         });
     };
 
     // add data ke local storage
-    static addPaket(kursus){
-        const belajar = paketKursus.getPaket();
+    static addPaket(produk){
+        const produk_exercise = paketProduk.getPaket();
 
-        belajar.push(kursus);
+        produk_exercise.push(produk);
 
-        localStorage.setItem('belajar', JSON.stringify(belajar));
+        localStorage.setItem('produk_exercise', JSON.stringify(produk_exercise));
     };
 
     // hapus data di local storage
     static deleteData(harga){  
-        const belajar = paketKursus.getPaket(); // mengapa pakai parameter harga karena di js tidak ada id 
+        const produk_exercise = paketProduk.getPaket(); // mengapa pakai parameter harga karena di js tidak ada id 
 
-        belajar.forEach(function(kursus,index){
-            if(kursus.harga === harga){
-                belajar.splice(index,1);
+        produk_exercise.forEach(function(produk,index){
+            if(produk.harga === harga){
+                produk_exercise.splice(index,1);
             };
         });
-        localStorage.setItem('belajar', JSON.stringify(belajar));
+        localStorage.setItem('produk_exercise', JSON.stringify(produk_exercise));
     }; 
 };
 
-document.addEventListener('DOMContentLoaded', paketKursus.tampilKursus);
+document.addEventListener('DOMContentLoaded', paketProduk.tampilProduk);
 
-document.getElementById('paket-kursus').addEventListener('submit', function(e){
-    const   paket = document.getElementById('paket').value,
-            jenis = document.getElementById('jenis').value,
-            harga = document.getElementById('harga').value
+document.getElementById('paket-produk').addEventListener('submit', function(e){
+    const   nama = document.getElementById('nama').value,
+            harga = document.getElementById('harga').value,
+            kategori = document.getElementById('kategori').value
+            
 
-    const kursus = new Kursus(paket, jenis, harga);
+    const produk = new Produk(nama, harga, kategori);
 
     const ui = new UI();
 
-    if (paket === "" || jenis === "" || harga ==="") {
+    if (nama === "" || kategori === "" || harga ==="") {
         alert("mohon isi data terlebih dahulu !")
     } else {
-        ui.simpanData(kursus);
+        ui.simpanData(produk);
         alert("terimakasih data telah disimpan");
-        paketKursus.addPaket(kursus);
+        paketProduk.addPaket(produk);
         ui.clearFields();
     }
 
     e.preventDefault(); // mencegah terjadi reload DOM 
 });
 
-document.getElementById('data-paket').addEventListener('click', function(e){
-
+document.getElementById('data-produk').addEventListener('click', function(e){
     const ui = new UI();
 
-    ui.deleteKursus(e.target);
-
+    ui.deleteProduk(e.target);
     alert ("apakah benar data ingin dihapus");
-    paketKursus.deleteData(e.target.parentElement.previusElementSibling.textContent);
+    let str = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    // paketProduk.deleteData(e.target.parentElement.parentElement.previusElementSibling.textContent);
+    paketProduk.deleteData(str.slice(4));
     // previusElementSibling --> mengembalikan element yang sebelumnya yang ditentukan
     e.preventDefault();
+    location.reload();
 });
 
